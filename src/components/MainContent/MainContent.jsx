ebbba23f11338566e0bbb2df986c09face0289fa
kusumm <kusumkatwal74@gmail.com>
2024-05-21 19:04:53 +0545

    Main content added

---
+++ src/components/MainContent/MainContent.jsx
@@ -0,0 +1,12 @@
+import "./MainContent.css";
+import ProductList from "../ProductList/ProductList";
+
+ function MainContent() {
+    return(
+        <div className="main-content">
+            <ProductList/>
+        </div>
+    );
+}
+
+export default MainContent;
